f333f37dc2e1d4a2054c942f9951f2d78d406dba
Moises_Salazar <42578698+MoisesSalazar@users.noreply.github.com>
2024-12-02 16:55:36 -0500

    Update 2024_12_01_225520_add_concept_to_transactions.php

--- database/migrations/2024_12_01_225520_add_concept_to_transactions.php
+++ database/migrations/2024_12_01_225520_add_concept_to_transactions.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            $table->string('concept')->nullable()->after('amount');
+           // $table->string('concept')->nullable()->after('amount');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            $table->dropColumn('concept');
+        //    $table->dropColumn('concept');
         });
     }
 };
